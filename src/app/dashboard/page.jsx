0c81ed2172849395a05fe8e9b8f591ca0de28b01
Surapat Hoknoo <surapat.ho@mail.wu.ac.th>
2025-01-22 15:57:39 +0700

    11th commit

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
 import Navbar_DB from "@/components/Navbar_DB";
 import Footer from "@/components/Footer";
+import { useState, useEffect } from "react";
 import PieChart from "@/components/PieChart";
 import SubjectCard from "@/components/SubjectCard";
 
@@ -29,10 +29,6 @@ export default function DashboardPage() {
         { name: "Subject B", age: 21, height: 181, stressLevel: "High" },
         { name: "Subject C", age: 18, height: 177, stressLevel: "Low" },
         { name: "Subject D", age: 19, height: 180, stressLevel: "Normal" },
-        { name: "Subject E", age: 22, height: 181, stressLevel: "Low" },
-        { name: "Subject F", age: 20, height: 177, stressLevel: "Medium" },
-        { name: "Subject G", age: 22, height: 170, stressLevel: "High" },
-        { name: "Subject H", age: 21, height: 180, stressLevel: "Normal" },
     ];
 
     const stressLevelColors = {
@@ -53,12 +49,10 @@ export default function DashboardPage() {
     return (
         <div className="min-h-screen flex flex-col bg-gray-100">
             <Navbar_DB />
-
             <div className="flex flex-wrap px-8">
-                {/* Summary Section */}
                 <div className="w-full md:w-1/2 p-4">
                     <div className="bg-white rounded-lg p-10 shadow-md">
-                        <h2 className="text-3xl text-blue-800 font-bold mb-4">Summary</h2>
+                        <h2 className="text-3xl text-navy mb-4">Summary</h2>
                         <div className="mt-8 w-full h-auto">
                             <PieChart data={summaryData} />
                         </div>
@@ -78,30 +72,22 @@ export default function DashboardPage() {
                         </div>
                     </div>
                 </div>
-
-                {/* Stress Levels Section */}
                 <div className="w-full md:w-1/2 p-4">
                     <div className="bg-white rounded-lg p-10 shadow-md">
-                        <h2 className="text-3xl text-blue-800 font-bold mb-4">Stress Levels</h2>
+                        <h2 className="text-3xl text-navy font-bold mb-4">Stress Levels</h2>
                         <div className="grid grid-cols-2 gap-4 overflow-y-auto" style={{ maxHeight: "510px" }}>
-                            {subjects
-                                .sort((a, b) => {
-                                    const stressOrder = { High: 1, Medium: 2, Low: 3, Normal: 4 };
-                                    return stressOrder[a.stressLevel] - stressOrder[b.stressLevel];
-                                })
-                                .map((subject, index) => (
-                                    <SubjectCard
-                                        key={`${subject.name}-${index}`}
-                                        subject={subject}
-                                        color={stressLevelColors[subject.stressLevel]}
-                                        onMoreClick={handleMoreClick}
-                                    />
-                                ))}
+                            {subjects.map((subject, index) => (
+                                <SubjectCard
+                                    key={`${subject.name}-${index}`}
+                                    subject={subject}
+                                    color={stressLevelColors[subject.stressLevel]}
+                                    onMoreClick={handleMoreClick}
+                                />
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
-
             <Footer />
         </div>
     );
